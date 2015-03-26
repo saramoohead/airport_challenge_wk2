5bad1012d9a13fe96ce7c93040edb07e5ce481f9
Sara OC <saramoo@hotmail.com>
2015-03-26 22:37:17 +0000

    passing unit tests for airport storing and releasing a plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,22 @@
 class Airport
+  attr_reader :store
+
   def initialize
     @empty
+    @store = []
   end
 
   def empty?
     @empty = true
+  end
+
+  def land! plane
+    plane.land!
+    store << plane
+  end
+
+  def take_off! plane
+    plane.take_off!
+    store.delete(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,24 @@ require 'airport'
 
 describe Airport do
   let(:airport) { Airport.new }
+  let(:plane) { double :plane }
+
+  before do
+    allow(plane).to receive :land!
+    allow(plane).to receive :take_off!
+  end
 
   it 'is empty when created' do
     expect(airport).to be_empty
+  end
+
+  it 'can store a plane' do
+    airport.land!(plane)
+    expect(airport.store).to eq [plane]
+  end
+
+  it 'can release a plane' do
+    airport.take_off!(plane)
+    expect(airport.store).not_to eq [plane]
   end
 end
