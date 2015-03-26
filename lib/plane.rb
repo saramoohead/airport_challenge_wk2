a35d0905cb469c29d53f8693923f433c7c392f83
Sara OC <saramoo@hotmail.com>
2015-03-26 21:14:46 +0000

    passing unit test for taking off

--- lib/plane.rb
+++ lib/plane.rb
@@ -14,4 +14,8 @@ class Plane
   def landed?
     @landed = true
   end
+
+  def take_off
+    @flying = true
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,12 +3,18 @@ require 'plane'
 describe Plane do
   let(:plane) { Plane.new }
 
-  it 'to be flying when created' do
+  it 'is flying when created' do
     expect(plane).to be_flying
   end
 
-  it 'can change its status to landed' do
+  it 'can land and change its status to landed' do
     plane.land
     expect(plane).to be_landed
+  end
+
+  it 'can take off' do
+    plane.land
+    plane.take_off
+    expect(plane).to be_flying
   end
 end
